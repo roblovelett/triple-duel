2327dca434f391ca34a6b32994becfcb17896b13
Fernando Baz <Fernando@BazNakauma-Macbook-Pro-2.local>
2017-05-03 15:32:16 -0500

    fixed powers, added code to prev mobile scroll

--- components/Main.js
+++ components/Main.js
@@ -180,12 +180,16 @@ var Main = React.createClass({
                         //var _class = "card_wrapper card col m12 c" + _this.state.player;
                         var _class = "card c" + _this.state.player;
                         hand_html[i] = (
-                            <div className={_class} data-card={i}>
-                                <img className="card-image" src={_this.state.hand[i].image_url}/>
-                                <div className="powers">
+                          <div className={_class} data-card={i}>
+                            <img className="card-image" src={_this.state.hand[i].image_url}/>
+                            <div className='powers'>{_this.state.hand[i].up}</div>
+                            <div className='powers'>{_this.state.hand[i].right}</div>
+                            <div className='powers'>{_this.state.hand[i].down}</div>
+                            <div className='powers'>{_this.state.hand[i].left}</div>
+                                {/*<div className="powers">
                                     {_this.state.hand[i].up}<br/>{_this.state.hand[i].left}
-                                    + {_this.state.hand[i].right}<br/>{_this.state.hand[i].down}
-                                </div>
+                                    &nbsp; &nbsp; &nbsp;{_this.state.hand[i].right}<br/>{_this.state.hand[i].down}
+                                </div>*/}
                                 {/*BIG WTF*/}
                                 {/*<div className="powers">
                                     <div className="powers-top">{_this.state.hand[i].up}</div>
@@ -195,7 +199,7 @@ var Main = React.createClass({
                                     </div>
                                     <div className="powers-bottom">{_this.state.hand[i].down}</div>
                                 </div>*/}
-                            </div>
+                          </div>
                         );
                     }
                 }
@@ -223,10 +227,14 @@ var Main = React.createClass({
                                 <div className={_class} data-card={i}>
                                     <img className="card-image" src={_this.state.board_game[i].image_url}/>
                                     {/*changes right here*/}
-                                    <div className="powers">
+																		<div className='powers'>{_this.state.hand[i].up}</div>
+                            				<div className='powers'>{_this.state.hand[i].right}</div>
+                            				<div className='powers'>{_this.state.hand[i].down}</div>
+                            				<div className='powers'>{_this.state.hand[i].left}</div>
+                                    {/*<div className="powers">
                                         {_this.state.board_game[i].up}<br/>{_this.state.board_game[i].left}
                                         + {_this.state.board_game[i].right}<br/>{_this.state.board_game[i].down}
-                                    </div>
+                                    </div>*/}
                                     {/*BIG WTF*/}
                                     {/*<div className="powers">
                                         <div className="powers-top">{_this.state.hand[i].up}</div>
